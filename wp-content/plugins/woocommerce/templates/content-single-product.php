0d273801443086d47ba45055550815d9ce6ed65a
thanhdd <thanhdd@lifetimetech.vn>
2013-09-27 14:13:37 +0700

    modify heading

--- wp-content/plugins/woocommerce/templates/content-single-product.php
+++ wp-content/plugins/woocommerce/templates/content-single-product.php
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 
 <div itemscope itemtype="http://schema.org/Product" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<h1 itemprop="name" class="product_title entry-title">Chi tiết sản phẩm <?php the_title(); ?></h1>
+	<h1 itemprop="name" class="product_title entry-title"><?php the_title(); ?></h1>
 	<?php
 		/**
 		 * woocommerce_show_product_images hook
